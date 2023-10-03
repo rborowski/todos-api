803875397ff888fe98c0870302b886716cef75b2
Rafał Borowski <moderator9781@gmail.com>
2023-10-03 19:42:46 +0200

    Create patch and delete functions

--- routes/todos.routes.js
+++ routes/todos.routes.js
@@ -8,4 +8,9 @@ router.get("/", todosController.getAllTodos)
 
 router.post("/", todosController.addTodo)
 
+router.patch("/:id", todosController.updateTodo)
+
+router.delete("/:id", todosController.deleteTodo)
+
+
 module.exports = router
